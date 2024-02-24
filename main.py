35b66d08ca810a67a8ef9dec4d68ab2bf84ba3d9
volodya <brave0wolf2010@gmail.com>
2024-02-24 10:09:41 +0100

    fsdf23

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import requests
 import lxml
 from bs4 import BeautifulSoup
 
-url = "https://kups.club/"
 
 session = requests.Session()
 header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
-
+for j in range(1, 6):
+    url = f"https://cash-backer.club/shops?page={j}"
 response = session.get(url, headers=header)
 print(response)
 soup = BeautifulSoup(response.text,"lxml")
 
-all_product = soup.find('div', class_="row mt-4")
-print(len(all_product))
-print(all_product.text)
+all_cashback = soup.find('div', class_="row mt-4")
 
-products = all_product.find_all('div',class_="col-lg-4 col-md-4 col-sm-6 portfolio-item ")
+cashback = all_cashback.find_all('div',class_="col-lg-3 col-md-3 col-12 menu")
+for elem in cashback:
+    price = elem.find("p", class_="card-text").text
+    price = price[price.find(":")+1:price.find("₽")]
+    title = elem.find("h3", class_="card-title").text.strip()
+    with open("cash-backer.txt", "a", encoding="utf-8") as file:
+        file.write(f"{title} ------>>>>\n")
+        print(price)
+
+
+
+
+
 
 
